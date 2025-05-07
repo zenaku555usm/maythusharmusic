7c7712b312fd71445fa15ba2009ee2d28ebeb746
zenaku555usm <pyaesone40usm@gmail.com>
2025-05-07 13:17:25 +0630

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/zenaku560usm/maythusharmusic",
+    "https://github.com/zenaku555usm/maythusharmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
